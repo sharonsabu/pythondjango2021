25bd87b70d5e5e38a0245101e802d1766d2df6e6
Sharon <sharonsabu100@gmail.com>
2021-01-28 11:18:56 +0530

    adding decisionmaking programs

---
+++ flowcontrols/decisionmaking/level_one.py
@@ -0,0 +1,14 @@
+# we use if...else condition to decide which to be printed depending on condition
+
+#if(condition):
+    #(after a tab space(not normal space hacve to use tab space))stmt1
+    #stmt2
+#else:
+    #stmt3
+
+
+age=int(input("enter age"))
+if(age>=18):
+    print("you are eligible for voting")
+else:
+    print("you are not eligible for voting")
---
+++ flowcontrols/decisionmaking/true_or_false.py
@@ -0,0 +1,6 @@
+num1=int(input("enter the 1st number"))
+num2=int(input("enter the 2nd number"))
+if(num1+num2==100)|(num1==100)|(num2==100):
+    print("true")
+else:
+    print("false")
