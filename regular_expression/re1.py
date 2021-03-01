ce9a69bf761f37f06524c901966deed45640c2ec
Sharon <sharonsabu100@gmail.com>
2021-03-01 10:29:49 +0530

    adding re

---
+++ regular_expression/re1.py
@@ -0,0 +1,12 @@
+from re import *
+
+pattern="ab"
+source="abababbbab"                        #a b a b a b b b a b
+matcher=finditer(pattern,source)           #0 1 2 3 4 5 6 7 8 9
+count=0
+for match in matcher:
+    print(match.start())
+    print(match.group())
+    count+=1
+print("")
+print(count)
