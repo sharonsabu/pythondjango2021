a611954d6e6fcbeb40a6716e5766af8497c24830
Sharon <sharonsabu100@gmail.com>
2021-03-01 10:38:45 +0530

    adding re

--- regular_expression/re2.py
+++ regular_expression/re2.py
@@ -1,6 +1,6 @@
 from re import *
 
-pattern="ab"     #checks the combination ab
+pattern='[ab]'   #checks a or b in each position
 source="abababbbab"                        #a b a b a b b b a b
 matcher=finditer(pattern,source)           #0 1 2 3 4 5 6 7 8 9
 count=0
---
+++ regular_expression/re3.py
@@ -0,0 +1,12 @@
+from re import *
+
+pattern='[ab]'
+source="ab Zk@9c"
+matcher=finditer(pattern,source)
+count=0
+for match in matcher:
+    print(match.start())
+    print(match.group())
+    count+=1
+print("")
+print(count)
