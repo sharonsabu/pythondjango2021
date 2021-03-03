d4795fc30fea97626b5031ee452c0c68d06688af
Sharon <sharonsabu100@gmail.com>
2021-03-03 10:44:58 +0530

    adding re

--- regular_expression/vv3.py
+++ regular_expression/vv3.py
@@ -1,9 +1,8 @@
-#rule for validating all gmail id's
-#recipient name-->upper or lowercase letters or digits 0-9,max 64
+#rule for validating phone number
 
 from re import *
 
-rule='[a-zA-Z0-9]{1,64}@gmail.com'
+rule=''
 variable_name=input("enter variable name")
 
 matcher=fullmatch(rule,variable_name)
