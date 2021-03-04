3472613c7265dc3c0e20645894817e53328cb08f
Sharon <sharonsabu100@gmail.com>
2021-03-04 09:33:47 +0530

    adding re

--- regular_expression/vv5.py
+++ regular_expression/vv5.py
@@ -1,1 +1,12 @@
-dd-mm--yy
+#dd-mm-yy
+
+from re import *
+
+rule='\d{0,31}[-]\d{0,12}[-]\d{4}'
+variable_name=input("enter variable name")
+
+matcher=fullmatch(rule,variable_name)
+if matcher!=None:
+    print("valid variable name")
+else:
+    print("invalid")
